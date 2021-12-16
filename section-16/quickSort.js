0e44a256885f0c32e2676d65cf39cd46543597bd
Gokhan T <trgtgokhan@gmail.com>
2021-12-17 00:52:09 +0300

    updated solution

--- section-16/quickSort.js
+++ section-16/quickSort.js
@@ -1,7 +1,11 @@
-function pivot(arr, startIndex = 0, endIndex = arr.length - 1) {
-  const pivotNumber = arr[0];
-  let pivotIndex = 0;
-  for (let i = 1; i < arr.length; i++) {
+function pivot(
+  arr,
+  startIndex = Math.floor(arr.length / 2),
+  endIndex = arr.length - 1
+) {
+  const pivotNumber = arr[startIndex];
+  let pivotIndex = startIndex;
+  for (let i = startIndex + 1; i <= endIndex; i++) {
     if (pivotNumber > arr[i]) {
       pivotIndex++;
       let temp = arr[i];
@@ -10,9 +14,21 @@ function pivot(arr, startIndex = 0, endIndex = arr.length - 1) {
     }
   }
   let temp = arr[pivotIndex];
-  arr[pivotIndex] = arr[0];
-  arr[0] = temp;
+  arr[pivotIndex] = arr[startIndex];
+  arr[startIndex] = temp;
   return pivotIndex;
 }
 
-console.log(pivot([8,2,1,6,10,9,3]));
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+
+const unsortedArr = [
+  8, 2, 1, 6, 10, 9, 3, 7, 12, 5, 22, 20, 17, 15, 13, 14, 19,
+];
+console.log(quickSort(unsortedArr));
